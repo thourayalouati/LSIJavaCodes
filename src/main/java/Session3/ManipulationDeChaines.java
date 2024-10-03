1b23fa45dd5f75842f89b5bd364fac015038a88d
Thouraya LOUATI <thouraya.louati@gmail.com>
2024-10-03 11:05:14 +0100

    add src commit "Codes sources Session 3"

--- src/main/java/Session3/ManipulationDeChaines.java
+++ src/main/java/Session3/ManipulationDeChaines.java
@@ -5,12 +5,20 @@ public class ManipulationDeChaines {
 
         String s1 = "Hello";
         String s2 = "Hello";
-        // Au lieu de créer une nouvelle chaîne, le compilateur référence la chaîne déjà présente dans la pool.
+
+        System.out.println(s1);
+        // Au lieu de créer une nouvelle chaîne, le compilateur référence la chaîne déjà présente
+        // dans la pool.
         if (s1 == s2) {
             System.out.println("s1 et s2 font référence au même objet !");
         } else {
             System.out.println("s1 et s2 font référence à des objets différents !");
         }
+
+
+
+
+
         // Chaîne de départ
         String chaine = "Bonjour, comment ça va ?";
 
--- src/main/java/Session3/TableauStatique.java
+++ src/main/java/Session3/TableauStatique.java
@@ -3,7 +3,7 @@ package Session3;
 public class TableauStatique {
     public static void main(String[] args) {
         int[] tableau = new int[5];
-
+        int [] scores={100,200,300,400};
         for (int i = 0; i < 5; i++) {
             tableau[i] = i + 1;
         }
@@ -12,6 +12,11 @@ public class TableauStatique {
         System.out.println("Contenu du tableau : ");
         for (int i = 0; i < 5; i++) {
             System.out.println(tableau[i]);
+
+        }
+        System.out.println("Contenu du tableau des scores : ");
+        for (int i = 0; i < 3; i++) {
+            System.out.println(scores[i]);
 
         }
     }
