6eb9b5c9150191cc8f9ef8b4ec63cbfc435d272e
Thouraya LOUATI <thouraya.louati@gmail.com>
2024-10-17 12:43:20 +0100

    add src commit "Codes sources Session 5"

---
+++ src/main/java/POO/Joueur.java
@@ -0,0 +1,65 @@
+package POO;
+
+class Joueur {
+
+    private String nom;
+    private String prenom;
+    private int age;
+
+
+    private String nationalite;
+    //Constructeur par défaut
+    public Joueur()
+    {
+        nom="";
+        prenom="";
+        age=0;
+        nationalite="";
+
+    }
+    //Constructeur avec paramètres
+    public Joueur(String nom, String prenom, int age, String nationalite)
+    {
+        this.nom=nom;
+        this.prenom=prenom;
+        this.age=age;
+        this.nationalite=nationalite;
+    }
+
+
+    public String getNom() {
+        return nom;
+    }
+
+    public String getPrenom() {
+        return prenom;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public String getNationalite() {
+        return nationalite;
+    }
+
+    public void setNom(String nom) {
+        this.nom = nom;
+    }
+
+    public void setPrenom(String prenom) {
+        this.prenom = prenom;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setNationalite(String nationalite) {
+        this.nationalite = nationalite;
+    }
+
+
+
+
+}
